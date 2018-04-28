ec6a33029f515a1e31c08ad298069ee363cb8513
pankajladhar <ladharpankaj@gmail.com>
2018-04-28 13:49:06 +0530

    Added style for footer component

--- src/Components/Base/Footer/index.js
+++ src/Components/Base/Footer/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
         <footer className="Footer">
-            <ul>
-                <li><a href="">Link 1</a></li>
-                <li><a href="">Link 1</a></li>
-                <li><a href="">Link 1</a></li>
+            <ul className="Footer__Menu">
+                <li className="Footer__Menu__Item">
+                    <a href="" className="Footer__Menu__Item__Link Footer__Menu__Item__Link--Search">
+                        <span className="sr-only">Link 1
+                    </span>
+                    </a>
+                </li>
+                <li className="Footer__Menu__Item">
+                    <a href="" className="Footer__Menu__Item__Link Footer__Menu__Item__Link--Plus">
+                        <span className="sr-only">Link 1
+                    </span>
+                    </a>
+                </li>
+                <li className="Footer__Menu__Item">
+                    <a href="" className="Footer__Menu__Item__Link Footer__Menu__Item__Link--Setting">
+                        <span className="sr-only">Link 1
+                    </span>
+                    </a>
+                </li>
             </ul>
         </footer>
     );
