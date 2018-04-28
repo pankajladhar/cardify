e7f19b15b5f95e8622f874773c9bb99937e73357
pankajladhar <ladharpankaj@gmail.com>
2018-04-28 11:24:39 +0530

    Updated Cards component in App component

--- src/Components/Compound/App/index.js
+++ src/Components/Compound/App/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from './../../Base/Header';
 import Footer from './../../Base/Footer';
+import Cards from './../../Compound/Cards'
 
 class App extends Component {
     render() {
         return (
             <div className="Container">
                 <Header />
+                <Cards />
                 <Footer />
             </div>
         );
